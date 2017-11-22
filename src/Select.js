819ac64fb6ec93ddb0aedfae5ec38c79a4ea7457
Xiao Lin <xiao@wayup.com>
2017-11-22 17:16:57 -0500

    Add keepCursorAtEnd prop for single value input
    This will allow single value input to position the cursor
    at the end of the input string instead of at the beginning.

--- src/Select.js
+++ src/Select.js
@@ -100,8 +100,14 @@ class Select extends React.Component {
 			'selectValue',
 		].forEach((fn) => this[fn] = this[fn].bind(this));
 
+		let inputValue = '';
+		if (!props.multi && props.keepCursorAtEnd && props.value) {
+			var valueArray = this.getValueArray(props.value);
+			inputValue = valueArray[0][props.labelKey];
+		}
+
 		this.state = {
-			inputValue: '',
+			inputValue: inputValue,
 			isFocused: false,
 			isOpen: false,
 			isPseudoFocused: false,
@@ -417,7 +423,7 @@ class Select extends React.Component {
 			isOpen: false,
 			isPseudoFocused: false,
 		};
-		if (this.props.onBlurResetsInput) {
+		if (this.props.onBlurResetsInput && !this.props.keepCursorAtEnd) {
 			onBlurredState.inputValue = this.handleInputValueChange('');
 		}
 		this.setState(onBlurredState);
@@ -428,6 +434,9 @@ class Select extends React.Component {
 
 		if (this.state.inputValue !== event.target.value) {
 			newInputValue = this.handleInputValueChange(newInputValue);
+			if (newInputValue.length === 0 && this.props.keepCursorAtEnd && !this.props.multi) {
+				this.popValue();
+			}
 		}
 
 		this.setState({
@@ -631,10 +640,11 @@ class Select extends React.Component {
 				}
 			});
 		} else {
+			const inputValue = this.props.keepCursorAtEnd ? value[this.props.labelKey] : updatedValue;
 			this.setState({
-				inputValue: this.handleInputValueChange(updatedValue),
+				inputValue: this.handleInputValueChange(inputValue),
 				isOpen: !this.props.closeOnSelect,
-				isPseudoFocused: this.state.isFocused,
+				isPseudoFocused: this.props.keepCursorAtEnd ? false : this.state.isFocused,
 			}, () => {
 				this.setValue(value);
 			});
@@ -878,6 +888,8 @@ class Select extends React.Component {
 			'aria-labelledby': this.props['aria-labelledby'],
 			'aria-owns': ariaOwns,
 			className: className,
+			autoComplete: 'off',
+			tabIndex: this.props.tabIndex,
 			onBlur: this.handleInputBlur,
 			onChange: this.handleInputChange,
 			onFocus: this.handleInputFocus,
@@ -1219,6 +1231,7 @@ Select.propTypes = {
 	instanceId: PropTypes.string,         // set the components instanceId
 	isLoading: PropTypes.bool,            // whether the Select is loading externally or not (such as options being loaded)
 	joinValues: PropTypes.bool,           // joins multiple values into a single form field with the delimiter (legacy mode)
+	keepCursorAtEnd: PropTypes.bool,      // keeps the cursor at the end of the input (for non multi selects)
 	labelKey: PropTypes.string,           // path of the label value in option objects
 	matchPos: PropTypes.string,           // (any|start) match the start or entire string when filtering
 	matchProp: PropTypes.string,          // (any|label|value) which option property to filter on
@@ -1287,6 +1300,7 @@ Select.defaultProps = {
 	inputProps: {},
 	isLoading: false,
 	joinValues: false,
+	keepCursorAtEnd: false,
 	labelKey: 'label',
 	matchPos: 'any',
 	matchProp: 'any',
